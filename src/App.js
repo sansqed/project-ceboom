2997a36d2e509353637850c95d3735655100f009
sansqed <44792e@gmail.com>
2022-12-02 04:32:37 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import {MapContainer, Marker, Popup, TileLayer, useMap} from "react-leaflet"
+
+import { useRef } from 'react';
+
+function App() {
+  return (
+    
+    <div className="App">
+      <link 
+        rel="stylesheet"
+        href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+      />
+
+      <MapContainer center={[10.3157, 123.8854]} zoom={12} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  );
+}
+
+export default App;
