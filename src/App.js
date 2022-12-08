55df8f8770d4272e4c99544a7933b0c8666a4765
sneedle <69748956+Kaelefor@users.noreply.github.com>
2022-12-08 23:35:49 +0800

    pushing basic navbar functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import {MapContainer, Marker, Popup, TileLayer, useMap} from "react-leaflet"
 
 import { useRef } from 'react';
+// import TestSidebar from './components/navbar/test';
+import Sidebar from './components/navbar/sidebar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './components/pages/Dashboard';
 
 function App() {
   return (
@@ -11,7 +15,15 @@ function App() {
         rel="stylesheet"
         href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
       />
+      {/* <testSidebar></testSidebar> */}
 
+      <BrowserRouter>
+        <Sidebar>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+          </Routes>
+        </Sidebar>
+      </BrowserRouter>
       <MapContainer center={[10.3157, 123.8854]} zoom={12} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
