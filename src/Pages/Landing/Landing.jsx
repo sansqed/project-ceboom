c36d6641568a8429733b444b299123bb50dbda26
johnochea <johnoochea@gmail.com>
2023-01-22 20:07:52 +0800

    landing page finished

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -7,18 +7,53 @@ import logo_one from "../../Assets/images/landing_page_cebu.png";
 
 const Landing = ({ children }) => {
 
+  /* Navigation between pages */
+  const navigate = useNavigate();
+  const navigateLogIn = () => navigate('/login');
+  const navigateAboutUs = () => navigate('/about-us');
+
   return(
     <div>
       <div className="landing-background">
         <div className="row">
-          <img alt="ceboom-logo" src={logo} className="ceboom-logo " />
-          <img alt="ceboom-logo" src={logo_one} className="ceboom-logo-one " />
-          <div className="col-1 mt-5">Home</div>
+
+          {/* CEBOOM logo */}
+          <img alt="ceboom-logo"
+          src={logo}
+          className="ceboom-logo"/>
+
+          {/* Cebu logo  */}
+          <img alt="ceboom-logo"
+          src={logo_one}
+          className="ceboom-logo-one"/>
+
+          <div className="nav-bar" align="right">
+            <a1 href="/">Home</a1>
+            <a href="/about-us">About</a>
+            <a href="/login">Login</a>
+          </div>
         </div>
+
         <div className="container">
           <div className="landing-main-text left">Find your way around.</div>
           <div className="landing-sub-text-one left">Find local businesses, view maps and get driving directions in CEBU.</div>
-          <div className="landing-sub-text-two left">Providing easy access to locations, landmarks, and roads, ultimately putting an emphasis on the user's "sense of direction."</div>
+          <div className="landing-sub-text-two left">
+              Providing easy access to locations, landmarks, and roads, ultimately putting an emphasis on the user's <span style = {{
+              color: 'darkorange'
+            }}>
+             "sense of direction."
+            </span>
+          </div>
+
+          <div className="login">
+            <button type="submit" onClick={navigateLogIn}>
+              <p>Log in</p>
+            </button>
+          </div>
+
+          <button className="learnmore" onClick={navigateAboutUs}>
+            <p>Learn more</p>
+          </button>
           
         </div>
       </div>
