623ec89d1dd29812ceac57210e077808358a408e
johnochea <johnoochea@gmail.com>
2023-01-22 21:33:43 +0800

    updated landing page & fixed errors from idk where

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,6 +1,6 @@
 import "./Landing.css"
-import React, { useState } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import logo from "../../Assets/images/ceboom_logo.png";
 import logo_one from "../../Assets/images/landing_page_cebu.png";
@@ -28,20 +28,20 @@ const Landing = ({ children }) => {
           className="ceboom-logo-one"/>
 
           <div className="nav-bar" align="right">
-            <a1 href="/">Home</a1>
+            <b href="/">Home</b>
             <a href="/about-us">About</a>
             <a href="/login">Login</a>
           </div>
         </div>
 
-        <div className="container">
+        <div>
           <div className="landing-main-text left">Find your way around.</div>
           <div className="landing-sub-text-one left">Find local businesses, view maps and get driving directions in CEBU.</div>
-          <div className="landing-sub-text-two left">
-              Providing easy access to locations, landmarks, and roads, ultimately putting an emphasis on the user's <span style = {{
-              color: 'darkorange'
-            }}>
-             "sense of direction."
+          <div className="landing-sub-text-two left">Providing easy access to locations, landmarks, and roads, ultimately putting an emphasis on the user's <span style={{
+            color: '#EB811E',
+            fontSize: '20px',
+            display: 'inline'}}> 
+              "sense of direction." 
             </span>
           </div>
 
