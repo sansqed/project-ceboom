7d94425376bff38a0063c4a15c3b8883f43f619d
gebmecod <jybravo@up.edu.ph>
2023-01-24 13:45:21 +0800

    Linked sign up button to registration and minor changes in registration

--- src/Pages/Registration/Registration.jsx
+++ src/Pages/Registration/Registration.jsx
@@ -11,17 +11,22 @@ const Registration = ({ children }) => {
             <img src={Cebu} alt="cebu" className="shadowCeb" />
             <h1 className="Text1">Find your way around.</h1>
             <h1 className="Text2">Find local businesses, view maps and get driving directions in CEBU</h1>
-            <h1 className="Text3">Providing easy access to locations, landmarks, and roads, ultimately putting an emphasis on the user's...</h1>
+            <h1 className="Text3">Providing easy access to locations,
+            landmarks, and roads, ultimately putting an emphasis on the user's...</h1>
             <h1 className="Text4">“Sense of Direction”</h1>
             <div className="form-background">
-                <button className="Google">
-                    <img src={Google} alt="Google" className="googleIcon"/>
-                    <text className="googleText">Sign up with Google</text>
-                </button>
-                <button className="Facebook">
-                    <img src={Facebook} alt="Facebook" className="facebookIcon"/>
-                    <text className="facebookText">Sign up with Facebook</text>
-                </button>
+                <a>
+                    <button className="Google">
+                        <img src={Google} alt="Google Icon" className="googleIcon"/>
+                        <text className="googleText">Sign up with Google</text>
+                    </button>
+                </a>
+                <a>
+                    <button className="Facebook">
+                        <img src={Facebook} alt="Facebook Icon" className="googleIcon"/>
+                        <text className="facebookText">Sign up with Facebook</text>
+                    </button>
+                </a>
                 <div className="inputBox">
                     <input type="text" required="required" />
                     <span>Full Name</span>
@@ -34,9 +39,11 @@ const Registration = ({ children }) => {
                     <input type="password" required="required" />
                     <span>Password</span>
                 </div>
-                <button className="create">
-                    Create Account
-                </button>
+                <a>
+                    <button className="create">
+                        Create Account
+                    </button>
+                </a>
                 <p className="link">
                     Already have an account? <a href="/login">Log in</a>
                 </p>
