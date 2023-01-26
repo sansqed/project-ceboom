2470de467110b5ae0d002585518abf0d385be239
thekatisdead <kaherbieto@up.edu.ph>
2023-01-26 18:28:37 +0800

    working CreateNode API

--- src/Pages/Map/Map.jsx
+++ src/Pages/Map/Map.jsx
@@ -94,6 +94,18 @@ const Map = ({ children }) => {
     setRoads(edges)
   },[])
 
+  async function createNodes(node){  
+    const response = await CreateNodes([node])
+
+    console.log(response)
+    if (response.data.status !== 201){
+      console.log("FAILED")
+    } else {
+      console.log("SUCCESS")
+    }
+
+  }
+
   const MapMarkerId = () =>{
     let map = useMap()
 
@@ -129,8 +141,23 @@ const Map = ({ children }) => {
   const typeIn = useRef(null)
   const locationIn = useRef(null)
 
-  const handleEditChange = () => {
-    console.log(nameIn.current.value, typeIn.current.value, locationIn.current.value)
+  async function handleEditChange (data) {
+    data.name = nameIn.current.value
+    data.location = locationIn.current.value
+    data.landmark_type = typeIn.current.value
+
+    console.log([data])
+    
+    const response = await CreateNodes([data])
+
+    // console.log(response)
+    // if (response.data.status !== 201){
+    //   console.log("FAILED")
+    // } else {
+    //   console.log("SUCCESS")
+    // }
+
+    
     // const {name, value, id} = e.target
 
     // if(tempData?.id == id){
@@ -193,7 +220,7 @@ const Map = ({ children }) => {
               <input id={data.leaflet_id} ref={typeIn}name='landmark_type' autoComplete='off'/>
               <label>Location</label>
               <input id={data.leaflet_id} ref={locationIn} name='location' autoComplete='off'/>  
-              <input id={data.leaflet_id} name={"submit-addlandmark"} type={"submit"} onClick={handleEditChange}/>
+              <input id={data.leaflet_id} name={"submit-addlandmark"} type={"submit"} onClick={()=>handleEditChange(data)}/>
             </Popup>
             <Marker 
               key={data.leaflet_id}
@@ -365,19 +392,7 @@ const Map = ({ children }) => {
     
   }
 
-  // async function createNodes(){  
-  //   console.log("HERE ")
-  //   console.log(nodes)
-  //   const response = await CreateNodes(nodes)
-
-  //   console.log(response)
-  //   if (response.data.status !== 201){
-  //     console.log("FAILED")
-  //   } else {
-  //     console.log("SUCCESS")
-  //   }
-
-  // }
+  
 
   // async function createRoads(){
   //   console.log("HERE ")
